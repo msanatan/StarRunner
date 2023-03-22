371f2561ea2328eb40784a1e2f75f48682312fe9
Marcus Sanatan <msanatan@gmail.com>
2023-03-21 22:19:59 -0400

    Adapt player code so it loads a model instead of a cube
    Might be a better default for me, time will tell

--- src/player.cpp
+++ src/player.cpp
@@ -2,21 +2,32 @@
 #include "raymath.h"
 #include "player.h"
 
-Player::Player(float x, float y, float z, float size)
+Player::Player(float x, float y, float z)
 {
   this->position = (Vector3){x, y, z};
-  this->size = size;
+  return;
+}
+
+Player::Player(float x, float y, float z, Model model)
+{
+  this->position = (Vector3){x, y, z};
+  this->model = model;
 }
 
 void Player::draw()
 {
-  DrawCube(this->position, this->size, this->size, this->size, RED);
-  DrawCubeWires(this->position, this->size, this->size, this->size, MAROON);
+  DrawModel(this->model, this->position, 1.0f, WHITE);
   return;
 }
 
 void Player::move(Vector3 velocity)
 {
   this->position = Vector3Add(this->position, velocity);
+  return;
+}
+
+void Player::setModel(Model model)
+{
+  this->model = model;
   return;
 }
--- src/player.h
+++ src/player.h
@@ -5,11 +5,13 @@ class Player
 {
 public:
   Vector3 position;
-  float size;
+  Model model;
 
-  Player(float x, float y, float z, float size);
+  Player(float x, float y, float z);
+  Player(float x, float y, float z, Model model);
   void draw();
   void move(Vector3 velocity);
+  void setModel(Model model);
 };
 
 #endif // !PLAYER_H
