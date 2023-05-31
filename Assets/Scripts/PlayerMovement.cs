95cac2a0a5da5b94510b1a1ec52c1cd920a0c6f0
Marcus Sanatan <msanatan@gmail.com>
2023-05-30 20:56:15 -0400

    Set up repo with basic ship movement
    We set up a URP game with the player ship model moving around.
    Very straightforward to import models + get them moving around of course.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 5.0f;
+    private CustomControls input = null;
+    private Vector3 playerVelocity = Vector3.zero;
+    private Rigidbody rb = null;
+    private Vector3 playerRotation = Vector3.zero;
+
+    private void Awake()
+    {
+        input = new CustomControls();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += OnMovementPerformed;
+        input.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= OnMovementPerformed;
+        input.Player.Movement.canceled -= OnMovementCancelled;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        playerVelocity = value.ReadValue<Vector3>();
+    }
+
+    private void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        playerVelocity = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = playerVelocity * playerSpeed;
+    }
+
+    private void Update()
+    {
+        playerRotation.x = rb.velocity.y / 2;
+        playerRotation.y = -rb.velocity.x / 2;
+        playerRotation.z = -rb.velocity.x * 2;
+        transform.Rotate(playerRotation * Time.deltaTime);
+    }
+}
